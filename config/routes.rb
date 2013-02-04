584abf0b59bf9d7c8929e17481800fd03852dd3a
Hugo Leeney <hugo.leeney@gmail.com>
2013-02-04 16:48:36 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root to: 'static_pages#home'
+
+  match '/signup', to: 'users#new'
+
   match '/about', to: 'static_pages#about'
   match '/help', to: 'static_pages#help'
   match '/contact', to: 'static_pages#contact'
