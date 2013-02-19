4b5374dce1b5e2761087c361f97b35029f415d87
Hugo Leeney <hugo.leeney@gmail.com>
2013-02-19 18:14:30 +0000

    finish user edit, update, index and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,20 +26,35 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+    it { should be_admin }
+  end
+
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
   end
+
   describe "when email is not present" do
     before { @user.email = "" }
     it { should_not be_valid }
   end
+
   describe "when name is too long" do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
+
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
@@ -49,6 +64,7 @@ describe User do
       end
     end
   end
+
   describe "when email format is valid" do
     it "should be valid" do
       addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
@@ -58,6 +74,7 @@ describe User do
       end
     end
   end
+
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
