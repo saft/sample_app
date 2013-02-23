ab98eb3a46ab107c352c35bbae3759ae1a9cbd67
Hugo Leeney <hugo.leeney@gmail.com>
2013-02-23 23:59:09 +0000

    add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -75,6 +75,16 @@ describe "AuthenticationPages" do
         before { put user_path(user) }
         specify { response.should redirect_to(signin_path) }
       end
+      describe "in the Microposts controller" do
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path)}
+        end
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost))}
+          specify { response.should redirect_to(signin_path)}
+        end
+      end
     end
     describe "as non-admin user" do
       let(:user) { FactoryGirl.create(:user) }
